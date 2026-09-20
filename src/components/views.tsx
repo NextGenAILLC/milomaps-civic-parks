c8a006e5f462a45deaf34f8600d6ef163e7dbbe4
GRAFUX GLOBAL <246871411+NextGenAILLC@users.noreply.github.com>
2026-09-20 18:39:23 -0500

    Enrich neighbor board, decisions, ballot context; keep sponsors grey; strip public ops chrome

--- src/components/views.tsx
+++ src/components/views.tsx
@@ -1,1 +1,6 @@
-PLACEHOLDER_LOAD_FROM_FILE
+export { SplitCard } from "./views-shared";
+export { ParkView } from "./park-view";
+export { VoteView } from "./vote-view";
+export { CheckinView } from "./checkin-view";
+export { FundView } from "./fund-view";
+export { LedgerView } from "./ledger-view";
